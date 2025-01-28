5b80f15028cf551ac3450ad0eca8eab4f55acb0d
MohammadHossein <mohamd.qorbani383@gmail.com>
2025-01-28 16:44:26 +0330

    init:user-models:create a user model

--- Syntax/accounts/admin.py
+++ Syntax/accounts/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from accounts.models import Comment, Like
+from accounts.models import Comment, Like , User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    search_fields = ('username', 'email' ,'first_name' ,'last_name')
 
 admin.site.register(Comment)
 admin.site.register(Like)
--- Syntax/accounts/models.py
+++ Syntax/accounts/models.py
@@ -1,7 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser , PermissionsMixin
 
-class User(models.Model):
-    username = models.CharField(max_length=255)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email,username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not username:
+            raise ValueError('Users must have an username')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email,username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            return ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, username, password, **extra_fields)
+
+
+class User(AbstractBaseUser , PermissionsMixin):
+    gender_choices = (('male','Male'),('female','Female'))
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=255 , unique=True)
+    password = models.CharField(max_length=128)
+    phone_number = models.CharField(max_length=15 , unique=True)
+    email = models.EmailField(unique=True)
+    birth_date = models.DateField(null=True, blank=True)
+    gender = models.CharField(choices=gender_choices, max_length=10, default='male', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    professional = models.IntegerField(max_length=10, blank=True, null=True)
+    followings_count = models.IntegerField(default=0)
+    followers_count = models.IntegerField(default=0)
+    posts_count = models.IntegerField(default=0)
+    comments_count = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name' , phone_number]
+
+    def __str__(self):
+        return self.username
 
 
 class Post(models.Model):
