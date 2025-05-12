031e470508dcf0e8a7906febf26a839785c45879
shahab-bozorgi <shahab.bozorgi007@gmail.com>
2025-05-12 20:10:47 +0330

    add: fb-user: add submit tasks for users

--- Syntax/apps/rooms/repositories/room_repo.py
+++ Syntax/apps/rooms/repositories/room_repo.py
@@ -7,10 +7,8 @@ from apps.rooms.models import Room, RoomTaskSubmission, RoomMembership
 class RoomListRepository:
     @staticmethod
     def get_membership(user, room):
-        try:
-            return RoomMembership.objects.get(user=user, room=room)
-        except RoomMembership.DoesNotExist:
-            raise PermissionDenied("User is not a member of this room.")
+        return RoomMembership.objects.get(user=user, room=room)
+
 
     @staticmethod
     def get_rooms_for_user(user):
@@ -18,13 +16,9 @@ class RoomListRepository:
 
     @staticmethod
     def post_tasks(membership, file, title: str, description: str = ""):
-        try:
-            return RoomTaskSubmission.objects.create(
-                membership=membership,
-                file=file,
-                title=title,
-                description=description,
-            )
-
-        except IntegrityError:
-            raise Exception("Submission failed due to a database error.")
+        return RoomTaskSubmission.objects.create(
+            membership=membership,
+            file=file,
+            title=title,
+            description=description,
+        )
--- Syntax/apps/rooms/views.py
+++ Syntax/apps/rooms/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import status
-from rest_framework.generics import ListAPIView
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.generics import ListAPIView, CreateAPIView, GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from yaml import serialize
 
-from apps.rooms.serializers import RoomListSerializer
+from apps.rooms.models import Room
+from apps.rooms.serializers import RoomListSerializer, SubmitTasksSerializer
 from apps.rooms.services.room_service import RoomListService
 
 
@@ -17,6 +20,35 @@ class RoomList(ListAPIView):
             return Response({"detail":"You're not in any room. "},status=status.HTTP_404_NOT_FOUND)
         serializer = RoomListSerializer(rooms, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class SubmitRoomTasks(GenericAPIView):
+    serializer_class = SubmitTasksSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request,room_id,  *args, **kwargs):
+        room = get_object_or_404(Room, id=room_id)
+        serializer = self.get_serializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        data = serializer.validated_data
+
+        try:
+            task = RoomListService.post_room_tasks(
+                user=request.user,
+                room=room,
+                file=data["file"],
+                title=data["title"],
+                description=data.get("description", "")
+            )
+            return Response({"detail": "Submission successful", "id": task.id}, status=status.HTTP_201_CREATED)
+
+        except PermissionDenied as e:
+            return Response({"detail": str(e)}, status=status.HTTP_403_FORBIDDEN)
+        except Exception as e:
+            return Response({"detail": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 
 
